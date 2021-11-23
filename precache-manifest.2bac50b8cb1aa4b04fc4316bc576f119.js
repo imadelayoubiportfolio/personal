88d4040a80a3254d9af50343aea438e6d02c9798
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-11-23 21:48:47 +0100

    Updates

---
+++ precache-manifest.2bac50b8cb1aa4b04fc4316bc576f119.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a8c97a052f282c5bc32da168918a38",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "aa48e97b314b6e790c3f",
+    "url": "/home/static/css/main.aea59a1d.chunk.css"
+  },
+  {
+    "revision": "dc2cc1d315e14b1911b7",
+    "url": "/home/static/js/2.5825628b.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/home/static/js/2.5825628b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa48e97b314b6e790c3f",
+    "url": "/home/static/js/main.f3b76cc7.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "81298fa5ead1c97d1dcdadef66208f85",
+    "url": "/home/static/media/Imad El Ayoubi.81298fa5.pdf"
+  },
+  {
+    "revision": "3d2dd1bdd0b603b1f0d901d301acd9e6",
+    "url": "/home/static/media/resume.3d2dd1bd.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
